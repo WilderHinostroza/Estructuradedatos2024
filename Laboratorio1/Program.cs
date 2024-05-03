74961294cd6c36e71605c9572fe2365575a1f804
Wilder Hinostroza <162522065+WilderHinostroza@users.noreply.github.com>
2024-05-03 17:32:37 -0500

    codigo lab1

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,10 @@
+﻿//sumar lo n primeros numeros
+
+var n = 2;
+var suma = 0;
+for (var i = 1; i <= n; i++)
+{
+    suma += i;
+}
+
+Console.WriteLine($"la suma de los {n} numeros naturales son : {suma}");
